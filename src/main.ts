6ab941b749b9b525edb8f1dbf2d315ccb97419b5
shangxinxiaoshuaige <1270046584@qq.com>
2023-05-04 22:43:45 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import store from "./store";
-import router  from './router';
+import router from './router';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
 
 const app = createApp(App);
 app.use(store);
 app.use(router);
+app.use(ElementPlus);
 app.mount('#app');
